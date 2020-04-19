60cb5bfee27ed31b880a409659170ea8ede0f22b
Arpit Satyal <arpited7@gmail.com>
2020-04-20 01:49:13 +0545

    follow/unfollow user that follows/follow me

--- controllers/userController.js
+++ controllers/userController.js
@@ -32,6 +32,53 @@ exports.follow = catchAsync(async(req, res, next) => {
     })
 })
 
+
+exports.followFollowingUser = catchAsync(async(req, res, next) => {
+    console.log(req.body)
+    User.findByIdAndUpdate({_id: req.user._id,
+    "following.userFollowed": { $ne: req.body.follower._id }}, {
+        $push: {
+            following: {
+                userFollowed: req.body.follower._id
+            }
+        }
+    }).then(async () => {
+        await User.findByIdAndUpdate({_id: req.body.follower._id, 
+            "followers.follower": { $ne: req.user._id } }, {
+                $push: {
+                    followers: {
+                        follower: req.user._id
+                    },
+                    notifications: {
+                        senderId: req.user._id,
+                        message: `${req.user.username} is now following you.`
+                    }
+                }
+            })
+            res.status(200).json({ msg: 'you have followed the user'})
+    })
+})
+
+exports.UnfollowFollowingUser = catchAsync(async(req, res, next) => {
+    console.log(req.body)
+    User.findByIdAndUpdate({_id: req.user._id}, {
+        $pull: {
+            following: {
+                userFollowed: req.body.follower._id
+            }
+        }
+    }).then(async () => {
+        await User.findByIdAndUpdate({_id: req.body.follower._id }, {
+                $pull: {
+                    followers: {
+                        follower: req.user._id
+                    }
+                }
+            })
+            res.status(200).json({ msg: 'you have unfollowed the user'})
+    })
+})
+
 exports.Unfollow = catchAsync(async(req, res, next) => {
     console.log(req.body)
     User.findByIdAndUpdate({_id: req.user._id}, {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,6 +7,8 @@ router.get('/get-users', authController.protect, userController.getAllUsers)
 router.get('/get-user/:id', authController.protect, userController.getUser)
 router.get('/get-user/:username', authController.protect, userController.getUserByName)
 router.post('/follow', authController.protect, userController.follow)
+router.post('/followFollowing', authController.protect, userController.followFollowingUser)
+router.post('/UnfollowFollowing', authController.protect, userController.UnfollowFollowingUser)
 router.post('/Unfollow', authController.protect, userController.Unfollow)
 router.get('/notifications', authController.protect, userController.getNotify)
 
